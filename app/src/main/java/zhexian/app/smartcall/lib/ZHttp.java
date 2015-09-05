9def5044b8561c0d189e4819937e96de2f6c0180
chenJunJie <hutckim@126.com>
2015-09-05 11:30:32 +0800

    修正一处可能导致InterruptedException 的bug

--- app/src/main/java/zhexian/app/smartcall/lib/ZHttp.java
+++ app/src/main/java/zhexian/app/smartcall/lib/ZHttp.java
@@ -39,12 +39,12 @@ public class ZHttp {
     }
 
     public static String getString(String urlStr) {
-        Response response = execute(urlStr);
-
-        if (response == null)
-            return null;
 
         try {
+            Response response = execute(urlStr);
+
+            if (response == null)
+                return null;
             return response.body().string();
         } catch (IOException e) {
             e.printStackTrace();
